2cdb31e05b4d02540192c2c1ec32b5286be54cf0
Danilo Josino <danilo@josino.com.br>
2023-01-13 19:03:15 -0300

    script de teste

---
+++ third_question.rb
@@ -0,0 +1,18 @@
+
+require_relative 'third_question/newsletter'
+require_relative 'third_question/observer'
+require_relative 'third_question/observers/phone_notify'
+require_relative 'third_question/observers/email_notify'
+require_relative 'third_question/subscription'
+
+subscription = Subscription.new('123', 'asdasd@asd.com')
+puts 'phone, email'
+subscription.create
+
+puts 'email'
+subscription.phone_number = nil
+subscription.create
+
+puts 'none'
+subscription.email = nil
+subscription.create
